6bd768fb2e83200f7ba43401471db866f621a2d2
Maulik <maulikprajapati712@gmail.com>
2022-07-24 10:45:45 +0530

    added cloudinary and installed express-fileulpload cloudinary js and connected back to front

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,16 +7,24 @@ const sendToken = require("../utils/jwtToken");
 const { validate } = require("../models/userModels");
 const sendEmail = require("../utils/sendEmail")
 const crypto = require("crypto");
+const cloudinary = require("cloudinary");
 
 exports.registerUser = catchasyncErrors( async(req, res, next)=>{
     
+    const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+        folder : "avatars",
+        width : 150,
+        crop : "scale",
+    });
+
+
     const {name, email, password} = req.body;
 
     const user = await User.create({
         name, email, password,
         avatar:{
-            public_id : "this is sample id",
-            url : "profilePicUrl"
+            public_id : myCloud.public_id ,
+            url : myCloud.secure_url,
         }
     })
 
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 
 const dotenv = require("dotenv");
+const cloudinary = require("cloudinary");
 const connectDataBase = require("./config/database");
 
 // handling uncaught exceptions
@@ -16,6 +17,13 @@ dotenv.config({path:"backend/config/config.env"});
 
 
 connectDataBase();
+
+cloudinary.config({
+    cloud_name : process.env.CLOUDINARY_NAME,
+    api_key : process.env.CLOUDINARY_API_KEY,
+    api_secret : process.env.CLOUDINARY_API_SECRET,
+    
+})
 
 const server = app.listen(process.env.PORT, ()=>{
     console.log(`server is working on : http://localhost:${process.env.PORT}`);
